dda9114534d8dda58cbd6b77f0926902efd577ef
Steve Odom <steve.odom@gmail.com>
2011-03-22 06:58:52 -0500

    added a thank you page

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	// $('#boy').delay(12000).animate({"right":"300px"}, 200000);
+	// $('form').submit(function() {
+	// 		var url = "http://gelatro.prefinery.com/api/v1/betas/60/testers"
+	// 		$.ajax({ contentType: "application/json",
+	// 							dataType: "json",
+	// 				 		  type: "POST",
+	// 							data: {api_key: "0b9b38ca08f94cebd8c07c8f73a2909b74dda88", tester: {email:"steve.odom+jquery@gmail.com"}},
+	// 						  url: url
+	// 					}); 
+	// 	  return false;
+	// 	});
+
 });
